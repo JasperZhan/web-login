6083d6ea63424a1de72ca214ab9c457de065ecc8
Jasper <zhanhanjie123@163.com>
2021-10-21 20:03:41 +0800

    Initial commit
    Spring Initializr

---
+++ src/main/java/cn/hzu/weblogin/WebLoginApplication.java
@@ -0,0 +1,13 @@
+package cn.hzu.weblogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLoginApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WebLoginApplication.class, args);
+  }
+
+}
---
+++ src/test/java/cn/hzu/weblogin/WebLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hzu.weblogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebLoginApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
