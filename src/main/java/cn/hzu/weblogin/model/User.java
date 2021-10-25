12d5e57436ec36eafc85d46e5c99d0361af5ee72
Jasper <zhanhanjie123@163.com>
2021-10-25 17:42:22 +0800

    modify User model

--- src/main/java/cn/hzu/weblogin/model/User.java
+++ src/main/java/cn/hzu/weblogin/model/User.java
@@ -3,14 +3,16 @@ package cn.hzu.weblogin.model;
 import lombok.Data;
 
 /**
- * model: User
- * 描述：用户实体
+ * @className: User
+ * @description: 用户实体，主要用于登录
+ * @author: Jsper Zhan
+ * @createDate: 2021/10/23
  */
 @Data
 public class User {
   int id;
-  String account_id;            //用户账户名
-  String phone;                 //用户手机号码
+  String tel;                   //用户手机号码
   String email;                 //用户邮箱号
   String password;              //用户密码
+  Boolean isCheck;              //判断用户是否已实名认证
 }
