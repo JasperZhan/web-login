f8d27bbee3cd1bcb76d65677051e59fb4ebe9b0e
Hzurang <2372047566@qq.com>
2021-10-28 22:00:12 +0800

    数据库字段名更改，user_info的地址字段密码错误，已更正

--- src/main/resources/db/login.sql
+++ src/main/resources/db/login.sql
@@ -15,24 +15,26 @@
 */
 
 SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+SET
+FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user`  (
-                         `id` int(11) NOT NULL AUTO_INCREMENT,
-                         `tel` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '手机号',
-                         `email` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '邮箱',
-                         `password` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '密码',
-                         `id_check` int(11) NULL DEFAULT 0 COMMENT '判断用户是否实名认证（0：未实名认证）',
-                         `face_token` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '人脸唯一标识',
-                         `third_login_type` int(11) NULL DEFAULT 0 COMMENT '三方登陆类型（0：未绑定三方登陆）',
-                         `third_login_id` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '三方登陆ID',
-                         `create_date` datetime(0) NULL DEFAULT NULL,
-                         `update_date` datetime(0) NULL DEFAULT NULL,
-                         PRIMARY KEY (`id`)
+CREATE TABLE `user`
+(
+    `id`               int(11) NOT NULL AUTO_INCREMENT,
+    `tel`              varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '手机号',
+    `email`            varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '邮箱',
+    `password`         varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '密码',
+    `id_check`         int(11) NULL DEFAULT 0 COMMENT '判断用户是否实名认证（0：未实名认证）',
+    `face_token`       varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '人脸唯一标识',
+    `third_login_type` int(11) NULL DEFAULT 0 COMMENT '三方登陆类型（0：未绑定三方登陆）',
+    `third_login_id`   varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '三方登陆ID',
+    `create_date`      datetime(0) NULL DEFAULT NULL,
+    `update_date`      datetime(0) NULL DEFAULT NULL,
+    PRIMARY KEY (`id`)
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
@@ -43,21 +45,23 @@ CREATE TABLE `user`  (
 -- Table structure for user_info
 -- ----------------------------
 DROP TABLE IF EXISTS `user_info`;
-CREATE TABLE `user_info`  (
-  `id` int(11) NULL DEFAULT NULL COMMENT '用户id',
-  `username` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户名',
-  `real_name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '真实姓名',
-  `id_card` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '身份证号',
-  `student_id_card` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '学生证号',
-  `addProvince` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '省份',
-  `addCity` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '市区',
-  `addDistruict` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '区/县',
-  `sex` int(11) NOT NULL DEFAULT -1 COMMENT '性别',
-  `birthday` datetime(0) NULL DEFAULT NULL COMMENT '生日'
+CREATE TABLE `user_info`
+(
+    `id`              int(11) NULL DEFAULT NULL COMMENT '用户id',
+    `username`        varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户名',
+    `real_name`       varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '真实姓名',
+    `id_card`         varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '身份证号',
+    `student_id_card` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '学生证号',
+    `addProvince`     varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '省份',
+    `addCity`         varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '市区',
+    `addDistrict`    varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '区/县',
+    `sex`             int(11) NOT NULL DEFAULT -1 COMMENT '性别，0是女生，1是男生',
+    `birthday`        datetime(0) NULL DEFAULT NULL COMMENT '生日'
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户信息表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user_info
 -- ----------------------------
 
-SET FOREIGN_KEY_CHECKS = 1;
+SET
+FOREIGN_KEY_CHECKS = 1;
