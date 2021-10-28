6ec4314f8787a7616171471c641bfb1687efbd34
Jasper <zhanhanjie123@163.com>
2021-10-28 10:02:34 +0800

    modify UserDao

--- src/main/java/cn/hzu/weblogin/dao/UserDao.java
+++ src/main/java/cn/hzu/weblogin/dao/UserDao.java
@@ -2,6 +2,7 @@ package cn.hzu.weblogin.dao;
 
 import cn.hzu.weblogin.model.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * @interfaceName: UserDao
@@ -32,25 +33,20 @@ public interface UserDao {
      * @return 用户对象
      */
     User getById(Integer id);
-    /**
-     * 根据用户名获取用户
-     *
-     * @param username 用户名
-     * @return 用户对象
-     */
-    User getByUsername(String username);
+
     /**
      * 根据手机号码获取用户
      *
      * @param user_phone 手机号码
      * @return 用户对象
      */
-    User getByPhone(String user_phone);
+    User getByPhone(@Param("user_phone") String user_phone);
+
     /**
      * 根据邮箱获取用户
      *
      * @param user_email 邮箱
      * @return 用户对象
      */
-    User getByEmail(String user_email);
+    User getByEmail(@Param("user_email") String user_email);
 }
