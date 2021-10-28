8eaf307450180d9186b560d5834fafedb94158a8
Jasper <zhanhanjie123@163.com>
2021-10-28 10:09:18 +0800

    登录功能

--- src/main/java/cn/hzu/weblogin/controller/LoginController.java
+++ src/main/java/cn/hzu/weblogin/controller/LoginController.java
@@ -6,12 +6,10 @@ import cn.hzu.weblogin.model.User;
 import cn.hzu.weblogin.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
 
 /**
  * @className: LoginController
@@ -19,94 +17,99 @@ import javax.validation.Valid;
  * @author: Hzu_rang
  * @createDate: 2021/10/25
  */
-@RestController
+@Controller
 public class LoginController {
     public static final String SESSION_NAME = "userInfo";
     @Autowired
     private UserService userService;
     @Autowired
     private UserDao userDao;
-    /**
-     * 用户注册
-     *
-     * @param user    传入注册用户信息
-     * @param errors  Validation的校验错误存放对象
-     * @param request 请求对象，用于操作session
-     * @return 注册结果
-     */
-    @PostMapping("/register")
-    public Result<User> register(@RequestBody @Valid User user, BindingResult errors, HttpServletRequest request) {
-        Result<User> result;
-        //如果校验有错，返回注册失败以及错误信息
-        if (errors.hasErrors()) {
-            result = new Result<>();
-            result.setResultFailed(errors.getFieldError().getDefaultMessage());
-            return result;
-        }
-        //调用注册服务
-        result = userService.register(user);
-        //如果注册成功，则设定session
-        if (result.isSuccess()) {
-            request.getSession().setAttribute(SESSION_NAME, result.getData());
-        }
-        return result;
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
     }
+
     /**
      * 用户登录
      *
-     * @param user    传入登录用户信息
-     * @param errors  Validation的校验错误存放对象
-     * @param request 请求对象，用于操作session
+     * @param loginWay 用户登录方式（0：手机登录；1：邮箱登录；2：人脸识别登录）
+     * @param userNum  用户登录账户（手机号码或者邮箱号码）
+     * @param userPw   用户登录密码
      * @return 登录结果
      */
-    @PostMapping("/login")
-    public Result<User> login(@RequestBody @Valid User user, BindingResult errors, HttpServletRequest request) {
-        Result<User> result;
-        //如果校验有错，返回登录失败以及错误信息
-        if (errors.hasErrors()) {
-            result = new Result<>();
-            result.setResultFailed(errors.getFieldError().getDefaultMessage());
-            return result;
+    @ResponseBody
+    @RequestMapping("/login/check")
+    public String loginCheck(String loginWay, String userNum, String userPw, HttpSession session) {
+
+        //返回信息
+        String retStr = "";
+        User user = new User();
+        Result<User> result = null;
+
+        System.out.println(loginWay);
+        int i = Integer.valueOf(loginWay).intValue();
+
+        switch (i) {
+            case 0:
+                user.setTel(userNum);
+                user.setPassword(userPw);
+                result = userService.loginByTel(user);
+                break;
+            case 1:
+                user.setEmail(userNum);
+                user.setPassword(userPw);
+                result = userService.loginByMail(user);
+                break;
         }
-        //调用登录服务
-        result = userService.login(user);
-        //如果登录成功，则设定session
+
         if (result.isSuccess()) {
-            request.getSession().setAttribute(SESSION_NAME, result.getData());
+            retStr = "location.href='/main'";
+        } else {
+            retStr = "alert('" + result.getMessage() + "')";
         }
-        return result;
+
+        return retStr;
+
+//        如果登录成功，则设定session
+//        if (result.isSuccess()) {
+//            request.getSession().setAttribute(SESSION_NAME, result.getData());
+//        }
+//        return result;
     }
+
+
     /**
      * 判断用户是否登录
      *
      * @param request 请求对象，从中获取session里面的用户信息以判断用户是否登录
      * @return 结果对象，已经登录则结果为成功，且数据体为用户信息；否则结果为失败，数据体为空
      */
-    @GetMapping("/islogin")
-    public Result<User> isLogin(HttpServletRequest request) {
-        HttpSession session = request.getSession();
-        Result<User> result = new Result<>();
-        //从session里面获取用户信息
-        User sessionUser = (User) session.getAttribute(SESSION_NAME);
-        //如果从session中获取用户信息为空，则说明没有登录
-        if (sessionUser == null) {
-            result.setResultFailed("用户未登录！");
-            return result;
-        }
-        //若用户登录，利用里面的信息去数据库查找并进行比对，保证信息正确性
-        User getUser = null;
-        try {
-            getUser = userDao.getByPhone(sessionUser.getTel());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        if (getUser == null || !getUser.getPassword().equals(sessionUser.getPassword())) {
-            result.setResultFailed("用户信息无效！");
-            return result;
-        }
-        result.setResultSuccess("用户已登录！", getUser);
-        return result;
-    }
+//    @GetMapping("/islogin")
+//    public Result<User> isLogin(HttpServletRequest request) {
+//        HttpSession session = request.getSession();
+//        Result<User> result = new Result<>();
+//        //从session里面获取用户信息
+//        User sessionUser = (User) session.getAttribute(SESSION_NAME);
+//        //如果从session中获取用户信息为空，则说明没有登录
+//        if (sessionUser == null) {
+//            result.setResultFailed("用户未登录！");
+//            return result;
+//        }
+//        //若用户登录，利用里面的信息去数据库查找并进行比对，保证信息正确性
+//        User getUser = null;
+//        try {
+//            getUser = userDao.getByPhone(sessionUser.getTel());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        if (getUser == null || !getUser.getPassword().equals(sessionUser.getPassword())) {
+//            result.setResultFailed("用户信息无效！");
+//            return result;
+//        }
+//        result.setResultSuccess("用户已登录！", getUser);
+//        return result;
+//    }
 
     /**
      * 用户登出
@@ -114,12 +117,12 @@ public class LoginController {
      * @param request 请求，用于操作session
      * @return 结果对象
      */
-    @GetMapping("/logout")
-    public Result logout(HttpServletRequest request) {
-        Result result = new Result();
-        //用户登出很简单，就是把session里面的用户信息设为null即可
-        request.getSession().setAttribute(SESSION_NAME, null);
-        result.setResultSuccess("用户退出登录成功！", null);
-        return result;
-    }
+//    @GetMapping("/logout")
+//    public Result logout(HttpServletRequest request) {
+//        Result result = new Result();
+//        //用户登出很简单，就是把session里面的用户信息设为null即可
+//        request.getSession().setAttribute(SESSION_NAME, null);
+//        result.setResultSuccess("用户退出登录成功！", null);
+//        return result;
+//    }
 }
