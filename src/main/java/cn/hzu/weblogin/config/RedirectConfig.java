e353f97151e3bd3cad55de96a2593d39bc884e09
Hzurang <2372047566@qq.com>
2021-10-29 11:06:41 +0800

    配置网页path重定向

---
+++ src/main/java/cn/hzu/weblogin/config/RedirectConfig.java
@@ -0,0 +1,14 @@
+package cn.hzu.weblogin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class RedirectConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+}
