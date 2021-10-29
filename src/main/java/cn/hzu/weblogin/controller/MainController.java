f08d6de53446fcde2fbdfbf08058055ce5265a30
Jasper <zhanhanjie123@163.com>
2021-10-29 09:26:30 +0800

    实现主页面的逻辑跳转，增加对登录、认证状态检查

--- src/main/java/cn/hzu/weblogin/controller/MainController.java
+++ src/main/java/cn/hzu/weblogin/controller/MainController.java
@@ -1,14 +1,55 @@
 package cn.hzu.weblogin.controller;
 
+import cn.hzu.weblogin.model.Result;
+import cn.hzu.weblogin.model.User;
+import cn.hzu.weblogin.model.vo.UserInfo;
+import cn.hzu.weblogin.service.UserInfoService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class MainController {
-    @ResponseBody
+
+    @Resource
+    UserInfoService userInfoService;
+
     @RequestMapping("/main")
-    public String hello() {
-        return "登录成功";
+    public String main(HttpSession session, Model model) {
+        return mainCheck(session, model);
+    }
+
+    @RequestMapping("/")
+    public String index(HttpSession session, Model model) {
+        return mainCheck(session, model);
+    }
+
+
+    private String mainCheck(HttpSession session, Model model) {
+        if (session.getAttribute("is_login") != null) {
+            if (session.getAttribute("is_check") != null) {
+                User user = new User();
+                user.setUserId((Integer) session.getAttribute("user_id"));
+                Result<UserInfo> result = userInfoService.show_userInfo(user);
+                if (result.isSuccess()) {
+                    UserInfo userInfo = result.getData();
+                    model.addAttribute("userInfo", userInfo);
+                    return "main";
+                } else {
+                    return "error";
+                }
+
+            } else {
+                return "verify";
+            }
+        } else {
+            return "login";
+        }
     }
 }
+
+
+
