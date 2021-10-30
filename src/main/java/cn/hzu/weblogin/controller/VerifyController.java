3ab1b4c09e797ad97f16df6b7f3244bf998c91d5
Jasper <zhanhanjie123@163.com>
2021-10-30 09:42:40 +0800

    添加认证页面进入判断，防止非法进入

--- src/main/java/cn/hzu/weblogin/controller/VerifyController.java
+++ src/main/java/cn/hzu/weblogin/controller/VerifyController.java
@@ -4,44 +4,75 @@ import cn.hzu.weblogin.model.Result;
 import cn.hzu.weblogin.model.User;
 import cn.hzu.weblogin.model.vo.UserInfo;
 import cn.hzu.weblogin.service.UserInfoService;
-import org.springframework.beans.factory.annotation.Autowired;
+import cn.hzu.weblogin.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
 @Controller
 public class VerifyController {
-    @Autowired
+    @Resource
     private UserInfoService userInfoService;
 
+    @Resource
+    private UserService userService;
+
     @RequestMapping("/verify")
-    public String verify() {
-        return "verify";
+    public String verify(HttpSession session) {
+        if (verifyStatus(session)) {
+            return "verify";
+        } else {
+            return "verifyError";
+        }
     }
 
-    @ResponseBody
     @RequestMapping("/verify/check")
-    public String loginCheck(String id_card, String real_name, HttpSession session) {
-        //返回信息
-        String retStr;
+    public String check(String id_card, String real_name, HttpSession session) {
+
+        if (!verifyStatus(session)) {
+            return "verifyError";
+        }
+
         UserInfo userInfo = new UserInfo();
-
-        Result<UserInfo> result = null;
-
-        System.out.println(id_card);
-
         userInfo.setId_Card(id_card);
         userInfo.setReal_name(real_name);
         userInfo.setId((Integer) session.getAttribute("id"));
-        System.out.println(userInfo.getId());
-        result = userInfoService.verify(userInfo);
+
+        Result<UserInfo> result = userInfoService.verify(userInfo);
+
         if (result.isSuccess()) {
-            retStr = "alert('" + result.getMessage() + "')";
+            return "verifySuccess";
         } else {
-            retStr = "alert('" + result.getMessage() + "')";
+            return "verifyError";
         }
-        return retStr;
+    }
+
+    /**
+     * 判断用户状态
+     * 只有已登录未注册的用户才允许进入实名认证页面
+     *
+     * @return boolean true：运行进入 false：非法进入
+     */
+    private boolean verifyStatus(HttpSession session) {
+        //用户未登录
+        if (session.getAttribute("is_login") == null) {
+            return false;
+        }
+        User user = new User();
+        user.setUserId((Integer) session.getAttribute("user_id"));
+        Result<User> result = userService.getUser(user);
+        if (result.isSuccess()) {
+            // 用户已认证
+            if (result.getData().getId_Check() == 1) {
+                return false;
+            }
+        } else {
+            // 由于 session 的 is_login 状态，是在登录后写入的，所以这里不需要判断用户是否存在
+            // 数据库访问出错，暂停进入
+            return false;
+        }
+        return true;
     }
 }
