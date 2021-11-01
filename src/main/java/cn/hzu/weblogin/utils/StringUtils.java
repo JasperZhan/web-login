d02cb7a9156f82235fa0fbdbe1c30b0898175b47
Jasper <zhanhanjie123@163.com>
2021-11-01 12:19:26 +0800

    手机号和邮箱号格式校验

--- src/main/java/cn/hzu/weblogin/utils/StringUtils.java
+++ src/main/java/cn/hzu/weblogin/utils/StringUtils.java
@@ -116,4 +116,24 @@ public class StringUtils extends org.apache.commons.lang3.StringUtils {
             return m.matches();
         }
     }
+
+    /**
+     * 校验邮箱是否有效
+     *
+     * @param email
+     * @return
+     */
+    public static boolean isEmail(String email) {
+        if (null == email || "".equals(email)) {
+            return false;
+        }
+        String regEx1 = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
+        Pattern p = Pattern.compile(regEx1);
+        Matcher m = p.matcher(email);
+        if (m.matches()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
