0240d6c35c49852c80a262ae3cd71a4c3a84ff31
Hzurang <2372047566@qq.com>
2021-11-04 11:57:05 +0800

    添加根据手机或邮箱更新用户、删除用户和找回用户的实现类

--- src/main/java/cn/hzu/weblogin/service/impl/UserServiceImpl.java
+++ src/main/java/cn/hzu/weblogin/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import cn.hzu.weblogin.model.Result;
 import cn.hzu.weblogin.model.User;
 import cn.hzu.weblogin.service.UserService;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -36,8 +37,8 @@ public class UserServiceImpl implements UserService {
             return result;
         }
         //加密储存用户的密码
-//        DigestUtils.md5Hex(user.getPassword())
-//        user.setPassword(user.getPassword());
+        String str = DigestUtils.md5Hex(user.getPassword());
+        user.setPassword(str);
         //存入数据库
         userDao.addByPhone(user);
         //返回用户数据，成功消息
@@ -60,8 +61,8 @@ public class UserServiceImpl implements UserService {
             return result;
         }
         //加密储存用户的密码
-//        DigestUtils.md5Hex(user.getPassword())
-//        user.setPassword(user.getPassword());
+        String str = DigestUtils.md5Hex(user.getPassword());
+        user.setPassword(str);
         //存入数据库
         userDao.addByMail(user);
         //返回用户数据，成功消息
@@ -84,10 +85,12 @@ public class UserServiceImpl implements UserService {
             result.setResultFailed("用户不存在！");
             return result;
         }
+        String str = getUser.getPassword();
+        String str2 = DigestUtils.md5Hex(user.getPassword());
         //比对密码（数据库取出用户的密码是加密的，因此要把前端传来的用户密码加密再比对）
-        if (!getUser.getPassword().equals(user.getPassword())) {
+        if (!str.equals(str2)) {
             System.out.println(getUser.getPassword());
-            System.out.println(user.getPassword());
+            System.out.println(DigestUtils.md5Hex(user.getPassword()));
             result.setResultFailed("手机号或者密码错误！");
             return result;
         }
@@ -110,8 +113,10 @@ public class UserServiceImpl implements UserService {
             result.setResultFailed("用户不存在！");
             return result;
         }
+        String str = getUser.getPassword();
+        String str2 = DigestUtils.md5Hex(user.getPassword());
         //比对密码（数据库取出用户的密码是加密的，因此要把前端传来的用户密码加密再比对）
-        if (!getUser.getPassword().equals(user.getPassword())) {
+        if (!str.equals(str2)) {
             System.out.println(getUser.getPassword());
             System.out.println(user.getPassword());
             result.setResultFailed("邮箱号或者密码错误！");
@@ -141,4 +146,85 @@ public class UserServiceImpl implements UserService {
         }
     }
 
+    @Override
+    public Result<User> findBackByTel(User user) {
+        Result<User> result = new Result<>();
+        User getUser = null;
+
+        try {
+            getUser = userDao.getByPhone(user.getTel());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (getUser == null) {
+            result.setResultFailed("用户不存在，请先注册！");
+            return result;
+        } else {
+            userDao.updateByPhone(getUser.getTel(), user.getPassword());
+            result.setResultSuccess("用户密码重置成功", getUser);
+            return result;
+        }
+    }
+
+    @Override
+    public Result<User> findBackByMail(User user) {
+        Result<User> result = new Result<>();
+        User getUser = null;
+
+        try {
+            getUser = userDao.getByEmail(user.getEmail());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (getUser == null) {
+            result.setResultFailed("用户不存在，请先注册！");
+            return result;
+        } else {
+            userDao.updateByEmail(getUser.getEmail(), user.getPassword());
+            result.setResultSuccess("用户密码重置成功", getUser);
+            return result;
+        }
+    }
+
+    @Override
+    public Result<User> addInfoByTel(User user) {
+        Result<User> result = new Result<>();
+        User getUser = null;
+
+        try {
+            getUser = userDao.getByPhone(user.getTel());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (getUser == null) {
+            userDao.updateEmail(user.getUserId(), user.getTel());
+            result.setResultSuccess("修改信息成功", user);
+        } else {
+            userDao.updateEmail(user.getUserId(), user.getTel());
+            userDao.deleteByPhone(getUser.getUserId(), user.getTel());
+            result.setResultSuccess("修改信息成功", user);
+        }
+        return result;
+    }
+
+    @Override
+    public Result<User> addInfoByMail(User user) {
+        Result<User> result = new Result<>();
+        User getUser = null;
+
+        try {
+            getUser = userDao.getByEmail(user.getEmail());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (getUser == null) {
+            userDao.updatePhone(user.getUserId(), user.getEmail());
+            result.setResultSuccess("修改信息成功", user);
+        } else {
+            userDao.updatePhone(user.getUserId(), user.getEmail());
+            userDao.deleteByEmail(getUser.getUserId(), user.getEmail());
+            result.setResultSuccess("修改信息成功", user);
+        }
+        return result;
+    }
 }
