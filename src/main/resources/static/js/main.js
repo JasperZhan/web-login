d3d15bc3165787ca07058fa0dc84291e19bac246
Hzurang <2372047566@qq.com>
2021-11-04 11:50:50 +0800

    完善信息的js

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -10,3 +10,28 @@ $("#logout").click(function () {
         }
     })
 });
+
+window.onload = function () {
+    var btn = document.getElementById("confirm")
+    let tel = document.getElementById("tel")
+    console.log(tel)
+    console.log(tel.value)
+    let email = document.getElementById("email")
+    console.log(email)
+    if (tel.value === "" || tel.value == null) {
+        btn.innerHTML="添加手机号";
+        $('#confirm').click(function () {
+            window.location.href="/addTel";
+        })
+    } else if (email.value === "" || email.value == null) {
+        btn.innerHTML="添加邮箱号";
+        $('#confirm').click(function () {
+            window.location.href="/addMail";
+        })
+    } else {
+        btn.innerHTML="信息完整";
+        $('#confirm').on('click', function () {
+            alert("当前信息已完整，无需修改")
+        })
+    }
+}
