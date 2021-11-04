fa8936f3095791460d404999ab78c2166df3f1d0
Hzurang <2372047566@qq.com>
2021-11-05 01:38:18 +0800

    添加了项目安全机制图片的展示功能

--- src/main/java/cn/hzu/weblogin/controller/DemonstrationController.java
+++ src/main/java/cn/hzu/weblogin/controller/DemonstrationController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DemonstrationController {
-    @RequestMapping("/dsc")
-    public String dsc() {
+    @RequestMapping("/demo")
+    public String demo() {
         return "dsc";
     }
 }
